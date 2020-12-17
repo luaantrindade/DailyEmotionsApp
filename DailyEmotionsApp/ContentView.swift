a67aff799caa238a58c28918e218960621cef713
luaantrindade <luanxpek@gmail.com>
2020-12-17 20:48:19 +0000

    DEA-1 Authetication and Error Screen

--- DailyEmotionsApp/ContentView.swift
+++ DailyEmotionsApp/ContentView.swift
@@ -7,6 +7,8 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 
 struct ContentView: View {
@@ -28,22 +30,40 @@ struct Home : View{
     
     @State var show = false
     @State var appeared: Double = 0.0
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
     
     var body: some View{
         
         NavigationView{
-            ZStack{
-                
-                NavigationLink(destination: SignUp(show: self.$show), isActive: self.$show) {
-                    
-                    Text("")
+            
+            VStack{
+                if self.status{
+                    Homescreen()
                 }
-                
-                Login(show: self.$show)
+                else{
+                    ZStack{
+                        
+                        NavigationLink(destination: SignUp(show: self.$show), isActive: self.$show) {
+                            
+                            Text("")
+                        }
+                        .hidden()
+                        
+                        Login(show: self.$show)
+                    }
+                }
             }
             .navigationBarTitle("")
             .navigationBarHidden(true)
             .navigationBarBackButtonHidden(true)
+            .onAppear {
+                NotificationCenter.default.addObserver(forName: NSNotification.Name("status"), object: nil, queue: .main) {
+                    (_) in
+                    
+                    self.status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                    
+                }
+            }
             
             
         }
@@ -56,6 +76,42 @@ struct Home : View{
     }
 }
 
+struct Homescreen : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Text("Logged Succefully")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.black.opacity(0.7))
+            
+            Button(action:{
+                
+                try! Auth.auth().signOut()
+                UserDefaults.standard.set(false, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                
+            })  {
+                
+                Text("Log out")
+                    .foregroundColor(.white)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 50)
+                
+            }
+            .background(Color("Color"))
+            .cornerRadius(20)
+            .padding(.top,25)
+        }
+        .padding(.horizontal, 25)
+        
+    }
+    
+}
+
+
 struct Login: View {
     //teste
     @State var color = Color.black.opacity(0.7)
@@ -63,113 +119,147 @@ struct Login: View {
     @State var pass = ""
     @State var visible = false
     @Binding var show : Bool
+    @State var alert = false
+    @State var error = ""
     /*
      @State var alert = false
      @State var error = ""*/
     
     var body: some View{
         
-        ZStack(alignment: .topTrailing){
-            
-            GeometryReader{_ in
+        ZStack{
+            ZStack(alignment: .topTrailing){
                 
-                VStack{
-                    //Login fields
-                    Image("logo")
-                    Text("Log in to account")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(self.color)
-                        .padding(.top, 35)
+                GeometryReader{_ in
                     
-                    //Email Field
-                    TextField("Email", text: self.$email)
+                    VStack{
+                        //Login fields
+                        Image("logo")
+                        Text("Log in to your account")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(self.color)
+                            .padding(.top, 35)
+                        
+                        //Email Field
+                        TextField("Email", text: self.$email)
+                            .padding()
+                            .autocapitalization(.none)
+                            .background(RoundedRectangle(cornerRadius: 20).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
+                            .padding(.top, 25)
+                        
+                        //Passoword Field
+                        HStack(spacing: 15){
+                            
+                            VStack{
+                                
+                                if self.visible{
+                                    
+                                    TextField("Password", text: self.$pass)
+                                        //Do not permit start the password with the first letter capitalizated.
+                                        .autocapitalization(.none)
+                                }
+                                else{
+                                    
+                                    SecureField("Password", text: self.$pass)
+                                        //Do not permit start the password with the first letter capitalizated.
+                                        .autocapitalization(.none)
+                                }
+                            }
+                            
+                            Button(action: {
+                                
+                                self.visible.toggle()
+                                
+                            }) {
+                                
+                                Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                    .foregroundColor(self.color)
+                            }
+                            
+                        }
                         .padding()
-                        .autocapitalization(.none)
-                        .background(RoundedRectangle(cornerRadius: 20).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
+                        .background(RoundedRectangle(cornerRadius: 20).stroke(self.pass != "" ? Color("Color") : self.color,lineWidth: 2))
                         .padding(.top, 25)
-                    
-                    //Passoword Field
-                    HStack(spacing: 15){
                         
-                        VStack{
-                            
-                            if self.visible{
+                        //Forget Password
+                        HStack{
+                            Button(action: {
                                 
-                                TextField("Password", text: self.$pass)
-                                    //Do not permit start the password with the first letter capitalizated.
-                                    .autocapitalization(.none)
-                            }
-                            else{
-                                
-                                SecureField("Password", text: self.$pass)
-                                    //Do not permit start the password with the first letter capitalizated.
-                                    .autocapitalization(.none)
+                            }){
+                                Spacer()
+                                Text("Forget password?")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color("Color"))
+                                    .padding(.bottom)
                             }
                         }
                         
-                        Button(action: {
-                            
-                            self.visible.toggle()
-                            
-                        }) {
-                            
-                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
-                                .foregroundColor(self.color)
+                        //Log in Button
+                        HStack{
+                            Button(action:{
+                                
+                                self.verify()
+                            }){
+                                
+                                Text("Log in")
+                                    .foregroundColor(.white)
+                                    .padding(.vertical)
+                                    .frame(width: UIScreen.main.bounds.width - 50)
+                                
+                            }
+                            .background(Color("Color"))
+                            .cornerRadius(20)
+                            .padding(.top,25)
                         }
                         
                     }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 20).stroke(self.pass != "" ? Color("Color") : self.color,lineWidth: 2))
-                    .padding(.top, 25)
-                    
-                    //Forget Password
-                    HStack{
-                        Button(action: {
-                            
-                        }){
-                            Spacer()
-                            Text("Forget password?")
-                                .fontWeight(.bold)
-                                .foregroundColor(Color("Color"))
-                                .padding(.bottom)
-                        }
-                    }
-                    
-                    //Log in Button
-                    HStack{
-                        Button(action:{
-                            
-                            
-                        }){
-                            
-                            Text("Log in")
-                                .foregroundColor(.white)
-                                .padding(.vertical)
-                                .frame(width: UIScreen.main.bounds.width - 50)
-                            
-                        }
-                        .background(Color("Color"))
-                        .cornerRadius(20)
-                        .padding(.top,25)
-                    }
-                    
+                    .padding(.horizontal, 25)
                 }
-                .padding(.horizontal, 25)
+                Button(action: {
+                    
+                    self.show.toggle()
+                    
+                }) {
+                    Text("Register")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Color"))
+                }
+                .padding()
             }
-            Button(action: {
+            
+            if self.alert{
+                ErrorView(alert: self.$alert, error: self.$error)
                 
-                self.show.toggle()
-                
-            }) {
-                Text("Register")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("Color"))
             }
-            .padding()
         }
         
+    }
+    
+    //Verify if the fields are filled properly
+    func verify(){
         
+        if self.email != "" && self.pass != "" {
+            
+            Auth.auth().signIn(withEmail: self.email,password: self.pass) {
+                (res,err) in
+                
+                if err != nil {
+                    self.error = err!.localizedDescription
+                    self.alert.toggle()
+                }
+                
+                print("success")
+                UserDefaults.standard.set(true, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                
+            }
+            
+        }else{
+            
+            self.error = "Please fill all the contents properly."
+            self.alert.toggle()
+        }
     }
     
     
@@ -184,8 +274,8 @@ struct SignUp: View {
     @State var visible = false
     @State var revisible = false
     @Binding var show : Bool
-    /* @State var alert = false
-     @State var error = ""*/
+    @State var alert = false
+    @State var error = ""
     
     var body: some View{
         
@@ -333,5 +423,55 @@ struct SignUp: View {
     }
     
     
+}
+
+struct ErrorView: View{
+    
+    @State var color =  Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var  error : String
+    
+    var body: some View{
+        GeometryReader{_ in
+            
+            VStack{
+                HStack{
+                    
+                    Text("Something went wrong!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 25)
+                
+                Text(self.error)
+                    .foregroundColor(self.color)
+                    .padding(.top)
+                    .padding(.horizontal, 25)
+                
+                //Cancel button
+                Button(action: {
+                    self.alert.toggle()
+                }) {
+                    Text("Cancel")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 120)
+                }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+                
+            }
+            .padding(.vertical, 25)
+            .frame(width: UIScreen.main.bounds.width - 70)
+            .background(Color.white)
+            .cornerRadius(15)
+            
+        }
+        .background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
+    }
 }
 
