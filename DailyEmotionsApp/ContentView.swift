7c03ba22494fe51834fd53e164393cb8166b4db9
luaantrindade <luanxpek@gmail.com>
2020-12-19 15:23:55 +0000

    DEA-1 Forgot Button, registering ok with firebase, verifying login, first part concluded

--- DailyEmotionsApp/ContentView.swift
+++ DailyEmotionsApp/ContentView.swift
@@ -9,11 +9,8 @@
 import SwiftUI
 import Firebase
 
-
-
 struct ContentView: View {
     var body: some View {
-        
         
         Home()
         
@@ -45,7 +42,7 @@ struct Home : View{
                         
                         NavigationLink(destination: SignUp(show: self.$show), isActive: self.$show) {
                             
-                            Text("")
+                            Text("ss")
                         }
                         .hidden()
                         
@@ -67,10 +64,10 @@ struct Home : View{
             
             
         }
-        .opacity(appeared)
-        .animation(Animation.easeInOut(duration: 1.0), value: appeared)
-        .onAppear {self.appeared = 1.0}
-        .onDisappear {self.appeared = 0.0}
+        //        .opacity(appeared)
+        ////        .animation(Animation.easeInOut(duration: 1.0), value: appeared)
+        ////        .onAppear {self.appeared = 1.0}
+        ////        .onDisappear {self.appeared = 0.0}
         
         
     }
@@ -110,7 +107,6 @@ struct Homescreen : View {
     }
     
 }
-
 
 struct Login: View {
     //teste
@@ -185,7 +181,7 @@ struct Login: View {
                         //Forget Password
                         HStack{
                             Button(action: {
-                                
+                                self.reset()
                             }){
                                 Spacer()
                                 Text("Forget password?")
@@ -236,6 +232,27 @@ struct Login: View {
         
     }
     
+    //Reset password
+    func reset(){
+        
+        if self.email != ""{
+            Auth.auth().sendPasswordReset(withEmail: self.email) { (err) in
+                
+                if err != nil{
+                    self.error = err!.localizedDescription
+                    self.alert.toggle()
+                    return
+                }
+                self.error = "RESET"
+                self.alert.toggle()
+            }
+            
+        }else{
+            self.error = "Email field is empty :("
+            self.alert.toggle()
+        }
+    }
+    
     //Verify if the fields are filled properly
     func verify(){
         
@@ -247,6 +264,7 @@ struct Login: View {
                 if err != nil {
                     self.error = err!.localizedDescription
                     self.alert.toggle()
+                    return
                 }
                 
                 print("success")
@@ -279,147 +297,164 @@ struct SignUp: View {
     
     var body: some View{
         
-        ZStack(alignment: .topLeading){
+        ZStack{
             
-            GeometryReader{_ in
+            ZStack(alignment: .topLeading){
                 
-                VStack{
-                    //Login fields
-                    Image("logo")
-                    Text("Log in to account")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(self.color)
-                        .padding(.top, 35)
+                GeometryReader{_ in
                     
-                    //Email Field
-                    TextField("Email", text: self.$email)
+                    VStack{
+                        //Login fields
+                        Image("logo")
+                        Text("Log in to account")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(self.color)
+                            .padding(.top, 35)
+                        
+                        //Email Field
+                        TextField("Email", text: self.$email)
+                            .padding()
+                            .autocapitalization(.none)
+                            .background(RoundedRectangle(cornerRadius: 20).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
+                            .padding(.top, 25)
+                        
+                        //Passoword Field
+                        HStack(spacing: 15){
+                            
+                            VStack{
+                                
+                                if self.visible{
+                                    
+                                    TextField("Password", text: self.$pass)
+                                        //Do not permit start the password with the first letter capitalizated.
+                                        .autocapitalization(.none)
+                                    
+                                }
+                                else{
+                                    
+                                    SecureField("Password", text: self.$pass)
+                                        //Do not permit start the password with the first letter capitalizated.
+                                        .autocapitalization(.none)
+                                    
+                                }
+                            }
+                            
+                            Button(action: {
+                                
+                                self.visible.toggle()
+                                
+                            }) {
+                                
+                                Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                    .foregroundColor(self.color)
+                            }
+                            
+                        }
                         .padding()
-                        .autocapitalization(.none)
-                        .background(RoundedRectangle(cornerRadius: 20).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
+                        .background(RoundedRectangle(cornerRadius: 20).stroke(self.pass != "" ? Color("Color") : self.color,lineWidth: 2))
                         .padding(.top, 25)
-                    
-                    //Passoword Field
-                    HStack(spacing: 15){
                         
-                        VStack{
+                        //Re-enter pass Field
+                        HStack(spacing: 15){
                             
-                            if self.visible{
+                            VStack{
                                 
-                                TextField("Password", text: self.$pass)
-                                    //Do not permit start the password with the first letter capitalizated.
-                                    .autocapitalization(.none)
+                                if self.revisible{
+                                    
+                                    TextField("Re-enter password", text: self.$repass)
+                                        //Do not permit start the password with the first letter capitalizated.
+                                        .autocapitalization(.none)
+                                }
+                                else{
+                                    
+                                    SecureField("Re-enter password", text: self.$repass)
+                                        //Do not permit start the password with the first letter capitalizated.
+                                        .autocapitalization(.none)
+                                }
                             }
-                            else{
+                            
+                            Button(action: {
                                 
-                                SecureField("Password", text: self.$pass)
-                                    //Do not permit start the password with the first letter capitalizated.
-                                    .autocapitalization(.none)
+                                self.revisible.toggle()
+                                
+                            }) {
+                                
+                                Image(systemName: self.revisible ? "eye.slash.fill" : "eye.fill")
+                                    .foregroundColor(self.color)
                             }
+                            
                         }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 20).stroke(self.repass != "" ? Color("Color") : self.color,lineWidth: 2))
+                        .padding(.top, 25)
                         
-                        Button(action: {
-                            
-                            self.visible.toggle()
-                            
-                        }) {
-                            
-                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
-                                .foregroundColor(self.color)
+                        
+                        //Register Button
+                        HStack{
+                            Button(action:{
+                                self.register()
+                                
+                            }){
+                                
+                                Text("Register")
+                                    .foregroundColor(.white)
+                                    .padding(.vertical)
+                                    .frame(width: UIScreen.main.bounds.width - 50)
+                            }
+                            .background(Color("Color"))
+                            .cornerRadius(20)
+                            .padding(.top,25)
                         }
                         
                     }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 20).stroke(self.pass != "" ? Color("Color") : self.color,lineWidth: 2))
-                    .padding(.top, 25)
-                    
-                    //Re-enter pass Field
-                    HStack(spacing: 15){
-                        
-                        VStack{
-                            
-                            if self.revisible{
-                                
-                                TextField("Re-enter password", text: self.$repass)
-                                    //Do not permit start the password with the first letter capitalizated.
-                                    .autocapitalization(.none)
-                            }
-                            else{
-                                
-                                SecureField("Re-enter password", text: self.$repass)
-                                    //Do not permit start the password with the first letter capitalizated.
-                                    .autocapitalization(.none)
-                            }
-                        }
-                        
-                        Button(action: {
-                            
-                            self.revisible.toggle()
-                            
-                        }) {
-                            
-                            Image(systemName: self.revisible ? "eye.slash.fill" : "eye.fill")
-                                .foregroundColor(self.color)
-                        }
-                        
-                    }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 20).stroke(self.repass != "" ? Color("Color") : self.color,lineWidth: 2))
-                    .padding(.top, 25)
-                    
-                    
-                    //Register Button
-                    HStack{
-                        Button(action:{
-                            
-                            
-                        }){
-                            
-                            Text("Register")
-                                .foregroundColor(.white)
-                                .padding(.vertical)
-                                .frame(width: UIScreen.main.bounds.width - 50)
-                        }
-                        .background(Color("Color"))
-                        .cornerRadius(20)
-                        .padding(.top,25)
-                    }
-                    
-                    
-                    //Register Button
-                    /* HStack(){
-                     
-                     Button(action:{
-                     
-                     
-                     }){
-                     
-                     Text("Register")
-                     .foregroundColor(.white)
-                     .padding(.vertical)
-                     .frame(width: UIScreen.main.bounds.width - 50)
-                     }
-                     .background(Color("Color"))
-                     .cornerRadius(20)
-                     .padding(.top,25)
-                     }
-                     */
-                    
+                    .padding(.horizontal, 25)
                 }
-                .padding(.horizontal, 25)
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Image(systemName:"chevron.left")
+                        .font(.title)
+                        .foregroundColor(Color("Color"))
+                }
+                .padding()
             }
-            Button(action: {
-                self.show.toggle()
-            }) {
-                Image(systemName:"chevron.left")
-                    .font(.title)
-                    .foregroundColor(Color("Color"))
+            
+            if self.alert{
+                ErrorView(alert: self.$alert, error: self.$error)
             }
-            .padding()
         }
         .navigationBarBackButtonHidden(true)
         
         
+    }
+    
+    func register(){
+        
+        if self.email != ""{
+            if self.pass ==  self.repass{
+                //Creating a new user
+                Auth.auth().createUser(withEmail: self.email, password: self.pass) {
+                    (res, err) in
+                    
+                    if err != nil {
+                        self.error = err!.localizedDescription
+                        self.alert.toggle()
+                        return
+                    }
+                    print("success")
+                    UserDefaults.standard.set(true, forKey: "status")
+                    NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                }
+            }
+            else{
+                self.error = "Password mismatch!"
+                self.alert.toggle()
+            }
+        }else{
+            self.error = "Please fill all the contents properly :)"
+            self.alert.toggle()
+        }
     }
     
     
@@ -437,7 +472,7 @@ struct ErrorView: View{
             VStack{
                 HStack{
                     
-                    Text("Something went wrong!")
+                    Text(self.error == "RESET" ? "Message" : "Something went wrong!")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(self.color)
@@ -446,7 +481,7 @@ struct ErrorView: View{
                 }
                 .padding(.horizontal, 25)
                 
-                Text(self.error)
+                Text(self.error == "RESET" ? "Password link has been sent" : self.error)
                     .foregroundColor(self.color)
                     .padding(.top)
                     .padding(.horizontal, 25)
@@ -455,7 +490,7 @@ struct ErrorView: View{
                 Button(action: {
                     self.alert.toggle()
                 }) {
-                    Text("Cancel")
+                    Text(self.error == "RESET" ? "OK" : "Cancel")
                         .foregroundColor(.white)
                         .padding(.vertical)
                         .frame(width: UIScreen.main.bounds.width - 120)
